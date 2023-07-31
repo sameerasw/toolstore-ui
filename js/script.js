8d646edb1176ecedc4255f53e20c65e0dfb40fc4
Sameera Sandakelum <68902530+sameerasw@users.noreply.github.com>
2023-07-31 16:54:18 +0530

    One page done without any graphs

--- js/script.js
+++ js/script.js
@@ -1,34 +1,46 @@
-//line
-var ctxL = document.getElementById("lineChart").getContext('2d');
-var myLineChart = new Chart(ctxL, {
+
+
+/* chart.js chart examples */
+
+// chart colors
+var colors = ['#007bff','#28a745','#333333','#c3e6cb','#dc3545','#6c757d'];
+
+/* large line chart */
+var chLine = document.getElementById("chLine");
+var chartData = {
+  labels: ["S", "M", "T", "W", "T", "F", "S"],
+  datasets: [{
+    data: [589, 445, 483, 503, 689, 692, 634],
+    backgroundColor: 'transparent',
+    borderColor: colors[0],
+    borderWidth: 4,
+    pointBackgroundColor: colors[0]
+  }
+//   {
+//     data: [639, 465, 493, 478, 589, 632, 674],
+//     backgroundColor: colors[3],
+//     borderColor: colors[1],
+//     borderWidth: 4,
+//     pointBackgroundColor: colors[1]
+//   }
+  ]
+};
+if (chLine) {
+  new Chart(chLine, {
   type: 'line',
-  data: {
-    labels: ["January", "February", "March", "April", "May", "June", "July"],
-    datasets: [{
-      label: "My First dataset",
-      data: [65, 59, 80, 81, 56, 55, 40],
-      backgroundColor: [
-        'rgba(105, 0, 132, .2)',
-      ],
-      borderColor: [
-        'rgba(200, 99, 132, .7)',
-      ],
-      borderWidth: 2
-    },
-    {
-      label: "My Second dataset",
-      data: [28, 48, 40, 19, 86, 27, 90],
-      backgroundColor: [
-        'rgba(0, 137, 132, .2)',
-      ],
-      borderColor: [
-        'rgba(0, 10, 130, .7)',
-      ],
-      borderWidth: 2
-    }
-    ]
-  },
+  data: chartData,
   options: {
+    scales: {
+      xAxes: [{
+        ticks: {
+          beginAtZero: false
+        }
+      }]
+    },
+    legend: {
+      display: false
+    },
     responsive: true
   }
-});
+  });
+}
