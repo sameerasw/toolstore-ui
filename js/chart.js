546b14e8b8f82adffb7e1922d71485e9efbd9ae7
Sameera Sandakelum <sameera.whoami@gmail.com>
2023-08-08 12:19:34 +0530

    Completed first 3 pages - refinements TBD

--- js/chart.js
+++ js/chart.js
@@ -4,10 +4,46 @@ type: 'line',
 data: {
     labels: ["January", " ", "February", " ", "April", " ", "May"],
     datasets: [{
-    label: 'Last 90 days',
+    label: 'Sales',
     backgroundColor: 'rgba(161, 198, 247, 0)',
     borderColor: 'rgb(47, 128, 237)',
     data: [300, 400, 200, 500, 800, 850, 800],
+    }]
+},
+options: {
+    scales: {
+    yAxes: [{
+        ticks: {
+        beginAtZero: true,
+        }
+    }]
+    }
+},
+});
+
+const chrt2 = document.getElementById("chart2").getContext('2d');
+      const myChart2 = new Chart(chrt2, {
+        type: 'doughnut',
+        data: {
+          labels: ["Active", "Off market (339)"],
+          datasets: [{
+            label: 'Orders',
+            data: [5284,339],
+            backgroundColor: ["#3870ff", "#22ff88"]
+          }]
+        },
+      });
+
+const ctx2 = document.getElementById("chart3").getContext('2d');
+const myChart3 = new Chart(ctx2, {
+type: 'line',
+data: {
+    labels: ["Jan", " ", "Feb", " ", "Apr", " ", "May"],
+    datasets: [{
+    label: 'Demand',
+    backgroundColor: 'rgba(161, 198, 247, 0)',
+    borderColor: 'rgb(22,255,55)',
+    data: [20, 100, 200, 500, 650, 700, 1200],
     }]
 },
 options: {
